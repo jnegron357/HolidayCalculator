ad4b5b6f15336cf7cd7b3c1bd51c24f7011b9bd8
Julio Negron <jnegron215@gmail.com>
2020-01-18 21:12:21 -0500

    Converted to a .Net Core 3.1 console app, did some refactoring. Test dates confirmed to be correct.

--- Program.cs
+++ Program.cs
@@ -4,17 +4,21 @@ public class Program
 {
 	public static void Main()
 	{
-		Console.WriteLine($"New Years day: {HolidayCalculator.NewYear}");
-		Console.WriteLine($"Memorial day: {HolidayCalculator.GetMemorialDay()}");
-		Console.WriteLine($"Labor day: {HolidayCalculator.GetLaborDay()}");
+		Console.WriteLine($"New Years day: {HolidayCalculator.NewYear.ToShortDateString()}");
+		Console.WriteLine($"Memorial day: {HolidayCalculator.GetMemorialDay().Value.ToShortDateString()}");
+		Console.WriteLine($"Labor day: {HolidayCalculator.GetLaborDay().Value.ToShortDateString()}");
+		Console.WriteLine($"Thanksgiving: {HolidayCalculator.GetThanksgiving().Value.ToShortDateString()}");
+		Console.WriteLine($"Chirstmas: {HolidayCalculator.Christmas.ToShortDateString()}");
 	}
 }
 
 public static class HolidayCalculator
 {
 	public static DateTime NewYear = new DateTime(DateTime.Now.Year, 1, 1);
+	public static DateTime Christmas = new DateTime(DateTime.Now.Year, 12, 25);
 	
-	public static DateTime? GetMemorialDay(){
+	public static DateTime? GetMemorialDay()
+	{
 		var memorialDay = TimeTools.GetLastDay(DateTime.Now.Year, 5, DayOfWeek.Monday);
 		return memorialDay == null ? DateTime.MinValue : memorialDay;
 	}
@@ -24,12 +28,17 @@ public static class HolidayCalculator
 		var laborDay = TimeTools.GetFirstDay(DateTime.Now.Year, 9, DayOfWeek.Monday);
 		return laborDay == null ? DateTime.MinValue : laborDay;
 	}
-	
+
+	public static DateTime? GetThanksgiving() 
+	{
+		var thanksgiving = TimeTools.GetLastDay(DateTime.Now.Year, 11, DayOfWeek.Thursday);
+		return thanksgiving == null ? DateTime.MinValue : thanksgiving;
+	}
+
 }
 
 public static class TimeTools
 {	
-
 	///<summary>Gets first occurance of given day in the given month and year</summary>
 	public static DateTime? GetFirstDay(int year, int month, DayOfWeek targetDay)
 	{
@@ -43,13 +52,13 @@ public static class TimeTools
 		}
 		return null;
 	}
-	
+
 	///<summary>Gets last occurance of given day in the given month and year</summary>
 	public static DateTime? GetLastDay(int year, int month, DayOfWeek targetDay)
 	{
 		var daysInMonth = DateTime.DaysInMonth(year, month);
 		//Loop from the last day to find our Monday much quicker
-		for (int day = 1; day <= daysInMonth; day++)
+		for (int day = daysInMonth; day > 0; day--)
 		{
 			DateTime currentDateTime = new DateTime(year, month, day);
 			//The first monday you hit will be the last monday of the month
